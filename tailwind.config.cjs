c54b4b9147e3680f2dde44c7f897836bef60b5fe
Gedeon <hello@gedeondoescode.com>
2023-08-15 08:54:31 -0400

    Start messing with card component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		borderRadius: {
-      'none': '0',
-      'sm': '.125rem',
-      DEFAULT: '.25rem',
-      'lg': '.5rem',
-      'full': '9999px',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    borderRadius: {
+      none: "0",
+      sm: ".125rem",
+      DEFAULT: ".25rem",
+      lg: ".5rem",
+      full: "9999px",
     },
-		colors: {
-      transparent: 'transparent',
-      primary: '#262626',
-			secondary: "#4731bd",
-			accent: "#8c8c8c"
-		},
-		container: {
-			center: true,
-		},
-		fontFamily: {
-			heading: ["Unbounded Variable", ...defaultTheme.fontFamily.sans],
-			sans: ["Be Vietnam Pro", ...defaultTheme.fontFamily.sans]
-		},
-		extend: {},
-	},
-	plugins: [],
-}
+    colors: {
+      transparent: "transparent",
+      primary: "#262626",
+      secondary: "#4731bd",
+      accent: "#8c8c8c",
+    },
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      heading: ["Unbounded Variable", ...defaultTheme.fontFamily.sans],
+      sans: ["Be Vietnam Pro", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
